763c3dc1790f045e7318cf62cd4aadac5cd0c937
shanoo2627 <shanookumar31222@gmail.com>
2022-07-25 13:08:18 +0530

    3rd

--- src/test/java/gitpractice/NewExample.java
+++ src/test/java/gitpractice/NewExample.java
@@ -5,8 +5,13 @@ public class NewExample {
 	public static void main(String[] args) {
 		
 		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
+		System.out.println("hi hlo");
+		System.out.println("hi how r u");
+		System.out.println("hi ");
+		System.out.println("hi how r u");
+		System.out.println("hi ");
+		
+		
 
 	}
 

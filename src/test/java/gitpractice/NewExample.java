b4bd3318ca45d310d4e782c252a6afcabd8706fd
shanoo2627 <shanookumar31222@gmail.com>
2022-07-25 12:56:53 +0530

    2nd commit

---
+++ src/test/java/gitpractice/NewExample.java
@@ -0,0 +1,13 @@
+package gitpractice;
+
+public class NewExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+
+	}
+
+}

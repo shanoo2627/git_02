898cfd1809d4ea6b6b471f7da29e81cecc68ada1
shanoo2627 <109574708+shanoo2627@users.noreply.github.com>
2022-07-25 15:21:06 +0530

    Update NewExample.java

--- src/test/java/gitpractice/NewExample.java
+++ src/test/java/gitpractice/NewExample.java
@@ -3,13 +3,8 @@ package gitpractice;
 public class NewExample {
 
 	public static void main(String[] args) {
-		//this is my 1st program
-		System.out.println("hi");
-		System.out.println("hi hlo");
-		System.out.println("hi how r u");
-		////this is my 2nd program
-		System.out.println("hi how r u");
-		//this is my 3rd program
+		
+		syatem.out.println("my name is:-");
 		
 		
 		
